3d2fcc8815656f6cd8a9814f895b2ad8a4e44090
DESKTOP-MFP8SQQ\xf <907700486@qq.com>
2016-12-03 17:58:37 +0800

    1.界面UI优化

--- app/src/main/java/com/bupt/adsystem/view/MainActivity.java
+++ app/src/main/java/com/bupt/adsystem/view/MainActivity.java
@@ -34,6 +34,8 @@ import com.serenegiant.usb.USBMonitor;
 import com.serenegiant.usb.UVCCamera;
 
 import java.lang.reflect.Method;
+import java.util.Calendar;
+import java.util.TimeZone;
 
 public class MainActivity extends Activity {
 
@@ -94,6 +96,7 @@ public class MainActivity extends Activity {
                 | WindowManager.LayoutParams.FLAG_KEEP_SCREEN_ON);
 
         this.setContentView(R.layout.activity_main);
+        initView();
         mContext = this;
         mTelephonyManager = (TelephonyManager) getSystemService(Context.TELEPHONY_SERVICE);
 
@@ -246,6 +249,44 @@ public class MainActivity extends Activity {
                 startActivity(i);
             }
         });
+        setStringData();
+    }
+
+    TextView timeTv,dateTv,weekTv;
+    private void initView(){
+        timeTv =(TextView)findViewById(R.id.time);
+        dateTv =(TextView)findViewById(R.id.date);
+        weekTv =(TextView)findViewById(R.id.week);
+    }
+
+    public void setStringData(){
+        final Calendar c = Calendar.getInstance();
+        c.setTimeZone(TimeZone.getTimeZone("GMT+8:00"));
+        String mYear = String.valueOf(c.get(Calendar.YEAR)); // 获取当前年份
+        String mMonth = String.valueOf(c.get(Calendar.MONTH) + 1);// 获取当前月份
+        String mDay = String.valueOf(c.get(Calendar.DAY_OF_MONTH));// 获取当前月份的日期号码
+        String mWay = String.valueOf(c.get(Calendar.DAY_OF_WEEK));
+        if("1".equals(mWay)){
+            mWay ="星期日";
+        }else if("2".equals(mWay)){
+            mWay ="星期一";
+        }else if("3".equals(mWay)){
+            mWay ="星期二";
+        }else if("4".equals(mWay)){
+            mWay ="星期三";
+        }else if("5".equals(mWay)){
+            mWay ="星期四";
+        }else if("6".equals(mWay)){
+            mWay ="星期五";
+        }else if("7".equals(mWay)){
+            mWay ="星期六";
+        }
+        weekTv.setText(mWay);
+        dateTv.setText(mYear+"-"+mMonth+"-"+mDay);
+
+        String hour = String.valueOf(c.get(Calendar.HOUR_OF_DAY));
+        String minute = String.valueOf(c.get(Calendar.MINUTE));
+        timeTv.setText(hour+" : "+minute);
     }
 
     @Override
