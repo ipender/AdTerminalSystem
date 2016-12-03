c2a4dbe84add1502a74fa5fb1987ff6fcff1346e
DESKTOP-MFP8SQQ\xf <907700486@qq.com>
2016-12-03 18:30:11 +0800

    完善工程

---
+++ rtmplibrary/src/main/java/net/ossrs/yasea/SrsCameraView.java
@@ -0,0 +1,229 @@
+package net.ossrs.yasea;
+
+import android.content.Context;
+import android.content.res.Configuration;
+import android.graphics.ImageFormat;
+import android.hardware.Camera;
+import android.os.Build;
+import android.util.AttributeSet;
+import android.util.Log;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+
+import java.io.IOException;
+import java.util.List;
+
+/**
+ * Created by leo.ma on 2016/9/13.
+ */
+public class SrsCameraView extends SurfaceView implements SurfaceHolder.Callback, Camera.PreviewCallback {
+    private Camera mCamera;
+
+    private int mPreviewRotation = 90;
+    private int mPreviewOrientation = Configuration.ORIENTATION_PORTRAIT;
+    private int mCamId = -1;
+    private PreviewCallback mPrevCb;
+    private byte[] mYuvPreviewFrame;
+    private int mPreviewWidth;
+    private int mPreviewHeight;
+
+    public interface PreviewCallback {
+        void onGetYuvFrame(byte[] data);
+    }
+
+    public SrsCameraView(Context context) {
+        this(context, null);
+    }
+
+    public SrsCameraView(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public void setCameraId(int id) {
+        mCamId = id;
+        setPreviewOrientation(mPreviewOrientation);
+    }
+
+    public void setPreviewOrientation(int orientation) {
+        mPreviewOrientation = orientation;
+        Camera.CameraInfo info = new Camera.CameraInfo();
+        Log.e("setPreviewOrientation","mCamId-?"+mCamId);
+        Camera.getCameraInfo(mCamId, info);
+        if (info.facing == Camera.CameraInfo.CAMERA_FACING_BACK) {
+            if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+                mPreviewRotation = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ? 270 : 90;
+            } else if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                mPreviewRotation = Build.VERSION.SDK_INT >= Build.VERSION_CODES.M ? 180 : 0;
+            }
+        } else if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+            if (orientation == Configuration.ORIENTATION_PORTRAIT) {
+                mPreviewRotation = 90;
+            } else if (orientation == Configuration.ORIENTATION_LANDSCAPE) {
+                mPreviewRotation = 0;
+            }
+        }
+    }
+
+    public int getCameraId() {
+        return mCamId;
+    }
+
+    public void setPreviewCallback(PreviewCallback cb) {
+        mPrevCb = cb;
+        getHolder().addCallback(this);
+    }
+
+    public int[] setPreviewResolution(int width, int height) {
+        mCamera = openCamera();
+
+        mPreviewWidth = width;
+        mPreviewHeight = height;
+        Camera.Size rs = adaptPreviewResolution(mCamera.new Size(width, height));
+        if (rs != null) {
+            mPreviewWidth = rs.width;
+            mPreviewHeight = rs.height;
+        }
+        mCamera.getParameters().setPreviewSize(mPreviewWidth, mPreviewHeight);
+        mYuvPreviewFrame = new byte[mPreviewWidth * mPreviewHeight * 3 / 2];
+        return new int[] { mPreviewWidth, mPreviewHeight };
+    }
+    
+    public boolean startCamera() {
+        if (mCamera == null) {
+            mCamera = openCamera();
+            if (mCamera == null) {
+                return false;
+            }
+        }
+
+        Camera.Parameters params = mCamera.getParameters();
+        int[] range = adaptPreviewFps(SrsEncoder.VFPS, params.getSupportedPreviewFpsRange());
+        params.setPreviewFpsRange(range[0], range[1]);
+        params.setPreviewSize(mPreviewWidth, mPreviewHeight);
+        params.setPreviewFormat(ImageFormat.NV21);
+        params.setFlashMode(Camera.Parameters.FLASH_MODE_OFF);
+        params.setWhiteBalance(Camera.Parameters.WHITE_BALANCE_AUTO);
+        params.setSceneMode(Camera.Parameters.SCENE_MODE_AUTO);
+
+        List<String> supportedFocusModes = params.getSupportedFocusModes();
+
+        if (!supportedFocusModes.isEmpty()) {
+            if (supportedFocusModes.contains(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE)) {
+                params.setFocusMode(Camera.Parameters.FOCUS_MODE_CONTINUOUS_PICTURE);
+            } else {
+                params.setFocusMode(supportedFocusModes.get(0));
+            }
+        }
+
+        mCamera.setParameters(params);
+
+        mCamera.setDisplayOrientation(mPreviewRotation);
+
+        mCamera.addCallbackBuffer(mYuvPreviewFrame);
+        mCamera.setPreviewCallbackWithBuffer(this);
+        try {
+            mCamera.setPreviewDisplay(getHolder());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        mCamera.startPreview();
+
+        return true;
+    }
+
+    public void stopCamera() {
+        if (mCamera != null) {
+            mCamera.setPreviewCallback(null);
+            mCamera.stopPreview();
+            mCamera.release();
+            mCamera = null;
+        }
+    }
+
+    private Camera openCamera() {
+        Camera camera;
+        if (mCamId < 0) {
+            Camera.CameraInfo info = new Camera.CameraInfo();
+            int numCameras = Camera.getNumberOfCameras();
+            int frontCamId = -1;
+            int backCamId = -1;
+            for (int i = 0; i < numCameras; i++) {
+                Camera.getCameraInfo(i, info);
+                if (info.facing == Camera.CameraInfo.CAMERA_FACING_BACK) {
+                    backCamId = i;
+                }
+                if (info.facing == Camera.CameraInfo.CAMERA_FACING_FRONT) {
+                    frontCamId = i;
+                    break;
+                }
+            }
+            if (frontCamId != -1) {
+                mCamId = frontCamId;
+            } else if (backCamId != -1) {
+                mCamId = backCamId;
+            } else {
+                mCamId = 0;
+            }
+        }
+        camera = Camera.open(mCamId);
+        return camera;
+    }
+
+    private Camera.Size adaptPreviewResolution(Camera.Size resolution) {
+        float diff = 100f;
+        float xdy = (float) resolution.width / (float) resolution.height;
+        Camera.Size best = null;
+        for (Camera.Size size : mCamera.getParameters().getSupportedPreviewSizes()) {
+            if (size.equals(resolution)) {
+                return size;
+            }
+            float tmp = Math.abs(((float) size.width / (float) size.height) - xdy);
+            if (tmp < diff) {
+                diff = tmp;
+                best = size;
+            }
+        }
+        return best;
+    }
+
+    private int[] adaptPreviewFps(int expectedFps, List<int[]> fpsRanges) {
+        expectedFps *= 1000;
+        int[] closestRange = fpsRanges.get(0);
+        int measure = Math.abs(closestRange[0] - expectedFps) + Math.abs(closestRange[1] - expectedFps);
+        for (int[] range : fpsRanges) {
+            if (range[0] <= expectedFps && range[1] >= expectedFps) {
+                int curMeasure = Math.abs(range[0] - expectedFps) + Math.abs(range[1] - expectedFps);
+                if (curMeasure < measure) {
+                    closestRange = range;
+                    measure = curMeasure;
+                }
+            }
+        }
+        return closestRange;
+    }
+
+    @Override
+    public void onPreviewFrame(byte[] data, Camera camera) {
+        mPrevCb.onGetYuvFrame(data);
+        camera.addCallbackBuffer(mYuvPreviewFrame);
+    }
+
+    @Override
+    public void surfaceChanged(SurfaceHolder holder, int format, int width, int height) {
+    }
+
+    @Override
+    public void surfaceCreated(SurfaceHolder arg0) {
+        if (mCamera != null) {
+            try {
+                mCamera.setPreviewDisplay(getHolder());
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    @Override
+    public void surfaceDestroyed(SurfaceHolder arg0) {
+    }
+}
