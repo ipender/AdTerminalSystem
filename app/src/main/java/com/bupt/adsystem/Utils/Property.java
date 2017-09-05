9e4efa317b47d49786903470d3a82b331d5f8098
Panda <pandengli888@gmail.com>
2017-09-05 11:01:13 +0800

    change config

--- app/src/main/java/com/bupt/adsystem/Utils/Property.java
+++ app/src/main/java/com/bupt/adsystem/Utils/Property.java
@@ -15,6 +15,7 @@ public class Property {
         RTMP_PULL_URL = context.getString(R.string.rtmp_pull_url);
         RTMP_PUSH_URL = context.getString(R.string.rtmp_push_url);
         SOAP_WEB_SERVICE_URL = context.getString(R.string.soap_web_service_url);
+        SOAP_METHOD_NAME = context.getString(R.string.soap_method_name);
         SOAP_NAME_SPACE = context.getString(R.string.soap_name_space);
         HTTP_GET_UPLOAD_INFO_URL_BASE = context.getString(R.string.http_get_upload_info_url);
         FILE_SERVER_URL = context.getString(R.string.file_server_url_base);
@@ -26,6 +27,7 @@ public class Property {
     public static String RTMP_PULL_URL;
 
     public static String SOAP_WEB_SERVICE_URL;
+    public static String SOAP_METHOD_NAME;
     public static String SOAP_NAME_SPACE;
     public static String HTTP_GET_UPLOAD_INFO_URL_BASE;
     public static String FILE_SERVER_URL;
